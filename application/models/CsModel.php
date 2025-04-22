fd5d49f7ecc7e4b61e69b5443ce1b0d5d4629ecf
normanardian <normanardian24@gmail.com>
2025-04-22 11:29:10 +0700

    update

--- application/models/CsModel.php
+++ application/models/CsModel.php
@@ -339,9 +339,9 @@ class CsModel extends M_Datatables
 	{
 		$this->db->select('a.*, b.service_name, c.nama_user');
 		$this->db->from('tbl_shp_order a');
-		$this->db->join('tb_service_type b', 'a.service_type=b.code');
-		$this->db->join('tbl_so d', 'a.id_so=d.id_so');
-		$this->db->join('tb_user c', 'd.id_sales=c.id_user');
+		$this->db->join('tb_service_type b', 'a.service_type=b.code', 'LEFT');
+		$this->db->join('tbl_so d', 'a.id_so=d.id_so', 'LEFT');
+		$this->db->join('tb_user c', 'd.id_sales=c.id_user', 'LEFT');
 		$this->db->where('a.id', $id);
 		$query = $this->db->get();
 		return $query;
